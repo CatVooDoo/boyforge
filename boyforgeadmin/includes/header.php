b8bd07b731cebe2812cb875c3721e60022ee3bfa
CatVooDoo <catvoodoo0@gmail.com>
2026-09-20 16:54:29 +0300

    fix: restore absolute paths for admin assets

--- boyforgeadmin/includes/header.php
+++ boyforgeadmin/includes/header.php
@@ -18,7 +18,7 @@ $csrfToken = getCsrfToken();
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/assets/css/admin.css?v=<?= time() ?>">
+  <link rel="stylesheet" href="/boyforgeadmin/assets/css/admin.css?v=<?= time() ?>">
 </head>
 <body>
 
